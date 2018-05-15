5265f8deef6e2381f13dcf0e687d62820b491c0f
Serhii Miroshnychenko <s.v.miroshnychenko@gmail.com>
2018-05-15 14:38:27 +0300

    removed unused import

--- src/main/java/selenium/core/WebDriverTestBase.java
+++ src/main/java/selenium/core/WebDriverTestBase.java
@@ -3,7 +3,6 @@ package selenium.core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
